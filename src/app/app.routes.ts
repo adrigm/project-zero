057cbe6001b64e758f98fe9699e1944f2b994c9b
adrigm <adrigm.admin@gmail.com>
2024-02-10 18:11:36 +0100

    Initial module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LineStatusComponent } from './dashboards/line-status/line-status.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'line-status', component: LineStatusComponent },
+  { path: '', redirectTo: '/line-status', pathMatch: 'full' },
+  { path: '**', redirectTo: '/line-status', pathMatch: 'full' }
+];
---
+++ src/app/dashboards/line-status/line-status.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-line-status',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './line-status.component.html',
+  styleUrl: './line-status.component.scss',
+})
+export class LineStatusComponent { }
