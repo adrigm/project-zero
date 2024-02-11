2dcbbdfe9300a617b9539378637080afccee3b9b
adrigm <adrigm.admin@gmail.com>
2024-02-11 13:03:02 +0100

    updated changes

--- src/app/dashboards/line-status/line-status.component.ts
+++ src/app/dashboards/line-status/line-status.component.ts
@@ -1,13 +1,93 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import * as L from 'leaflet';
+import { LatLngBoundsExpression } from 'leaflet';
+
 
 @Component({
   selector: 'app-line-status',
   standalone: true,
   imports: [
     CommonModule,
+    HttpClientModule
   ],
   templateUrl: './line-status.component.html',
   styleUrl: './line-status.component.scss',
 })
-export class LineStatusComponent { }
+export class LineStatusComponent implements OnInit {
+
+  public image: HTMLImageElement;
+  private map: L.Map;
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private async initMap() {
+    const imageUrl = '/assets/plano.png';
+
+    this.image = await this.loadImage(imageUrl);
+    console.log('img', this.image.width, this.image.height);
+
+    const aspectRatio = this.image.width / this.image.height;
+
+
+    const imageBounds: LatLngBoundsExpression = [[0, 0], [1000, 1000*aspectRatio]];
+    const imageBounds2: LatLngBoundsExpression = [[0, 0], [1000, 1000*aspectRatio]];
+
+    this.map = L.map('map', {
+      minZoom: -1,
+      maxZoom: 2,
+      zoom: 0,
+      // zoomDelta: 1,
+      inertia: true,
+      inertiaDeceleration: 0,
+      inertiaMaxSpeed: 0,
+      zoomSnap: 0,
+      boxZoom: true,
+      maxBounds: imageBounds2,
+      wheelDebounceTime: 0,
+      wheelPxPerZoomLevel: 1,
+      doubleClickZoom: false,
+      crs: L.CRS.Simple,
+      attributionControl: false,
+    });
+
+
+    L.imageOverlay(this.image.src, imageBounds).addTo(this.map);
+    this.map.fitBounds(imageBounds);
+
+
+    const c = L.circle([600, 300], {radius: 50}).addTo(this.map);
+    c.addEventListener('click', (e) => {
+      console.log('click', e);
+    });
+
+    let color = '#00aa00';
+    c.setStyle({color, fillOpacity: 0.3, opacity: 0.8});
+
+    setInterval(() => {
+      color = color === 'red' ? '#00aa00' : 'red';
+      c.setStyle({color});
+    }, 1000);
+
+
+
+    L.marker([500, 1000]).addTo(this.map);
+  }
+
+  private loadImage(src: string): Promise<HTMLImageElement> {
+    return new Promise((resolve, reject) => {
+      const imagen = new Image();
+      imagen.onload = () => resolve(imagen);
+      imagen.onerror = reject;
+      imagen.src = src;
+    });
+  }
+}
